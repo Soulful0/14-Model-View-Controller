a74b40c28ea19ef864f51f9186d5977c1d9513ae
Soul <joshgingold336@gmail.com>
2024-05-21 21:45:22 -0500

    restarting: restarting

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helper");
+
 const sequelize = require("./config/connection");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,9 +15,29 @@ const PORT = process.env.PORT || 3001;
 // Create the Handlebars.js engine object with custom helper functions
 const hbs = exphbs.create({ helpers });
 
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const userRoutes = require("./routes/api/userRoutes");
+const homeRoutes = require("./routes/homeRoutes");
+
+// Using routes
+app.use("/api/users", userRoutes);
+app.use("/", homeRoutes);
+
 // Inform Express.js which template engine we're using
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
